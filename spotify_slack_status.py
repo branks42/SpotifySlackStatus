9f93690907a9cc9a9b29dc708c0b27e2db063cb6
Justin Branco <jbranco@nd.edu>
2020-05-16 10:10:02 -0400

    Spotify Slack Status

---
+++ spotify_slack_status.py
@@ -0,0 +1,32 @@
+import spotipy
+import spotipy.util as util
+import time
+
+from update_status import update_status
+
+# Spotify App credentials
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+# Slack Credentials
+slack_token = ''
+# The second blank is filled in during the function call
+slack_url = 'https://slack.com/api/users.profile.set?token={}&profile={}'.format(slack_token)
+
+# This gets the user's authorization token
+# Parameters are username, scope, client id, client secret and redirect url
+# The CLIENT_ID, CLIENT_SECRET and REDIRECT_URL need to be setup in Spotify Developer Dashboard.
+token = util.prompt_for_user_token(
+    'YOU_SPOTIFY_USERNAME_HERE',
+    'user-read-playback-state',
+    CLIENT_ID,
+    CLIENT_SECRET,
+    'http://localhost:9090')
+
+
+spotify = spotipy.Spotify(auth=token)
+
+while True:
+    sleep_time, spotify = update_status(spotify, slack_url, token)
+    time.sleep(sleep_time)
+
---
+++ update_status.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import spotipy
+
+
+def update_status(spotify, slack_url, token):
+    """
+    Update the requesting user's Slack status.
+
+    Parameters
+    ----------
+    spotify: class
+        Spotipy class
+    slack_url : str
+        Url to post the new status
+    token : str
+        Spotify token
+
+    Returns
+    -------
+    The time to sleep (Length of current track - progress in song + 5 seconds)
+    """
+    try:
+        current_song = spotify.current_playback()
+    except spotipy.exceptions.SpotifyException:
+        # Spotify Authentication error, authorization resets after an hour
+        spotify = spotipy.Spotify(auth=token)
+        current_song = spotify.current_playback()
+
+    song_info = "{} - {}".format(current_song['item']['artists'][0]['name'],
+                                 current_song['item']['name'])
+
+    print("Changing song to {} \n".format(song_info))
+
+    # Change the status
+    sleep_time = (current_song['item']['duration_ms'] - current_song['progress_ms']) / 1000
+    print("Gonna sleep for {} seconds...\n".format(sleep_time))
+    
+    slack_status = {"status_text": song_info, "status_emoji": ":spotify:"}
+    response = requests.post(slack_url.format(json.dumps(slack_status)))
+
+    return sleep_time + 5, spotify
